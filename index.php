4411851af5c243865ee18158753ecd83f88d6ace
bluesky255255 <46342980+bluesky255255@users.noreply.github.com>
2021-01-18 15:56:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,16 @@
 
 <?php echo "Hello World!"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
-/*<?php phpinfo(); ?><br /><br />*/
+// <?php phpinfo(); ?><br /><br />
 <?php echo $_SERVER['HTTP_HOST']; ?><br /><br />
+  
+<?php
+$url = $_SERVER['HTTP_HOST'];
+$parse = parse_url($url);
+$host = $parse['host'];
+$host = str_ireplace('openshift-php-hello-world-git-demo.apps.', '', $host);
+echo $host;
+?>
 
 <?php $links = [];
   foreach($_ENV as $key => $value) {
